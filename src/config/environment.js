26bc56d412cd8808f6f9459c36363dc24a8203e3
danhdoan263 <doan.working@gmail.com>
2024-10-05 02:46:52 +0700

    enable dev mode with cross-env

--- src/config/environment.js
+++ src/config/environment.js
@@ -3,8 +3,11 @@ import 'dotenv/config'
 export const env = {
     MONGODB_URI: process.env.MONGODB_URI,
     MONGODB_NAME: process.env.MONGODB_NAME,
+
     APP_HOST: process.env.APP_HOST,
     APP_PORT: process.env.APP_PORT,
-    AUTHOR: process.env.AUTHOR
 
+    AUTHOR: process.env.AUTHOR,
+
+    BUILD_MODE: process.env.BUILD_MODE
 }
